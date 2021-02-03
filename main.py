f85b8471b122fae49b86755aa01afa7e9f9c4771
dany_ev3 <s.daniele01@gmail.com>
2021-02-03 02:04:59 +0100

    The GUI does things now. Not many but it does.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 import requests
 import tkinter as tk
+from tkinter import filedialog
 
 
 def convert_to_quest():
@@ -36,25 +37,46 @@ def convert_to_pc(folder_name):
     return f'{song.json()["key"]} ({song.json()["metadata"]["songName"]} - {song.json()["metadata"]["levelAuthorName"]})'
 
 
+def get_dir():
+    sel_dir = filedialog.askdirectory(master=frm)
+    song_dir.set(sel_dir)
+
+
+
+# Let's make this damned GUI.
 # Create the window and configure the grid
 window = tk.Tk()
 window.title("Beat Saber Song Converter")
+window.resizable(False, False)
 window.rowconfigure(0, weight=0)
 window.rowconfigure(1, weight=1, minsize=50)
 window.columnconfigure([0, 1], weight=1, minsize=5)
-# Cute groowy frame
+# Cute growoy frame
 frm = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=3)
-# Label and entry
-path_lbl = tk.Label(master=frm, text="Path:")
-path_ent = tk.Entry(master=frm, width=50)
-path_lbl.grid(row=0, column=0)
-path_ent.grid(row=0, column=1)
-# Buttons
+frm.columnconfigure(0, weight=1)
+frm.columnconfigure([1, 2], weight=2)
+# Directory selection button
+song_dir = tk.StringVar()
+btn_path = tk.Button(master=frm, text="Select the custom songs folder", command=get_dir)
+btn_path.grid(row=0, rowspan=2, column=0, padx=5, pady=5, sticky="wens")
+# Directory selection entry
+path_ent = tk.Entry(master=frm, width=100, textvariable=song_dir)
+path_ent.grid(row=0, column=1, columnspan=2, padx=5, pady=5)
+# Song number labels
+songs_quest = tk.StringVar(value=f"0 Quest songs found")
+songs_pc = tk.StringVar(value=f"0 PC songs found")
+lbl_quest = tk.Label(master=frm, textvariable=songs_quest)
+lbl_pc = tk.Label(master=frm, textvariable=songs_pc)
+lbl_quest.grid(row=1, column=1, sticky="wens")
+lbl_pc.grid(row=1, column=2, sticky="wens")
+# Render Frame
+frm.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky="we")
+# Conversion buttons
 btn_quest = tk.Button(master=window, text="PC -> Quest")
 btn_pc = tk.Button(master=window, text="Quest -> PC")
-frm.grid(row=0, columnspan=2, padx=5, pady=5, sticky="we")
 btn_quest.grid(row=1, column=0, padx=5, pady=5, sticky="wens")
 btn_pc.grid(row=1, column=1, padx=5, pady=5, sticky="wens")
-
+# Progress bar
+# SOON:tm:
 # GUI LET'S GOOOOOOOOOO
 window.mainloop()
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Beat Saber Song Converter
 An executable to... well... _convert Beat Saber songs_?\
 You can use this to change the names of your PC song's folders to use your song library on Quest!\
-You can also convert from Quest to PC in case you like your songs folder to look nice and tidy but PC beat saber doesn't actually care what the song folders are called.\
-Also going from Quest to PC **requires** internet access, going from PC to Quest doesn't.\
+You can also convert from Quest to PC in case you like your songs folder to look nice and tidy but PC Beat Saber doesn't actually care what the song folders are called.\
+Also going from Quest to PC **requires** internet access, going from PC to Quest doesn't.
 
 I'll put a tutorial now here when the thing works, I promise. ~~Maybe.~~
